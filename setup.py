d10dc8e971dd6278439ce9a9d65db46a4a532e19
Mikko Ohtamaa <mikko@opensourcehacker.com>
2012-11-21 22:52:56 +0200

    Back to development: 1.2

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.1'
+version = '1.2.dev0'
 
 setup(name='collective.portletalias',
       version=version,
-      description="",
+      description="Alias portlets from other locations to show on the current content in Plone CMS",
       long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
